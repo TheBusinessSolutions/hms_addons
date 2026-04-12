ef66c1ef76c81207a7fa855ab4ee57bedb509f7f
alihassanmostafa <alihassan.mostafa@gmail.com>
2026-04-12 07:58:33 +0200

    add the drug form

--- acs_hms/models/prescription.py
+++ acs_hms/models/prescription.py
@@ -234,7 +234,7 @@ class ACSPrescriptionLine(models.Model):
         ('line_note', "Note")], help="Technical field for UX purpose.")
     # This pulls the form name directly from the product
     drug_form_id = fields.Many2one('drug.form', string='Form', 
-    related='product_id.drug_form_id', readonly=True)
+    related='product_id.form_id', readonly=True)
  
     @api.onchange('product_id')
     def onchange_product(self):
